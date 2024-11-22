5ca65f0c770c01cd4789978602ce8eada364c395
Augusto Mattei Grohmann <108702302+Goldenkiuren@users.noreply.github.com>
2024-11-22 01:30:31 -0300

    Inicialização do projeto

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/augus/Desktop/WegasVideoEditor
+# Build directory: C:/Users/augus/Desktop/WegasVideoEditor
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
